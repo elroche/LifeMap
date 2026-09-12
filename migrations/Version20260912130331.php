c9d086b3634f70f2995fe0c1721a54535b85db43
Elea Roche <elroche@ensc.fr>
2026-09-12 16:04:46 +0200

    edit EventType Entity, edit base.html.twig

--- migrations/Version20260912130331.php
+++ migrations/Version20260912130331.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260910141537 extends AbstractMigration
+final class Version20260912130331 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,9 +21,7 @@ final class Version20260910141537 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE event CHANGE end_date end_date DATETIME DEFAULT NULL');
-        $this->addSql('ALTER TABLE framework ADD technology_id INT NOT NULL');
-        $this->addSql('ALTER TABLE framework ADD CONSTRAINT FK_9D766E194235D463 FOREIGN KEY (technology_id) REFERENCES technology (id)');
-        $this->addSql('CREATE INDEX IDX_9D766E194235D463 ON framework (technology_id)');
+        $this->addSql('ALTER TABLE event_type ADD color VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL, CHANGE username username VARCHAR(255) DEFAULT NULL, CHANGE pending_email pending_email VARCHAR(180) DEFAULT NULL, CHANGE email_change_token email_change_token VARCHAR(64) DEFAULT NULL, CHANGE email_change_requested_at email_change_requested_at DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT NULL');
     }
@@ -32,9 +30,7 @@ final class Version20260910141537 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE event CHANGE end_date end_date DATETIME DEFAULT \'NULL\'');
-        $this->addSql('ALTER TABLE framework DROP FOREIGN KEY FK_9D766E194235D463');
-        $this->addSql('DROP INDEX IDX_9D766E194235D463 ON framework');
-        $this->addSql('ALTER TABLE framework DROP technology_id');
+        $this->addSql('ALTER TABLE event_type DROP color');
         $this->addSql('ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT \'NULL\'');
         $this->addSql('ALTER TABLE user CHANGE pending_email pending_email VARCHAR(180) DEFAULT \'NULL\', CHANGE email_change_token email_change_token VARCHAR(64) DEFAULT \'NULL\', CHANGE email_change_requested_at email_change_requested_at DATETIME DEFAULT \'NULL\', CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_bin`, CHANGE username username VARCHAR(255) DEFAULT \'NULL\'');
     }
--- src/Entity/EventType.php
+++ src/Entity/EventType.php
@@ -16,6 +16,9 @@ class EventType
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $color = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +32,18 @@ class EventType
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): static
+    {
+        $this->color = $color;
 
         return $this;
     }
